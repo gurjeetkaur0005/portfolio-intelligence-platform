252656a0c00b411cb4e569be70d4dc0738fe1c0e
GurjeetKaur0022 <gurjeetkaursaini2001@gmail.com>
2026-08-05 19:51:40 +0530

    Fix Alembic import ordering

--- alembic/env.py
+++ alembic/env.py
@@ -1,30 +1,22 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+from src.database import models  # noqa: F401
+from src.database.base import DatabaseBase
 from src.database.config import get_database_url
+
+
+config = context.config
 
 config.set_main_option(
     "sqlalchemy.url",
     get_database_url(),
 )
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-from src.database.base import DatabaseBase
-from src.database import models  # noqa: F401
 
 target_metadata = DatabaseBase.metadata
 
